014a850b8bb226e07c7fb6205656ae8f0267615f
dukyeon ha <iribbonkr@gmail.com>
2021-03-28 21:20:57 +0900

    page

---
+++ src/page.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+// Layout
+const Main = React.lazy(() => import('./layout/Main'));
+const Sub = React.lazy(() => import('./layout/Sub'));
+
+//Page
+const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'));
+const Page = React.lazy(() => import('./views/pages/sub/Page'));
+
+
+const page = [
+  { path: '/', exact: true, name: 'Home' },
+  { path: '/dashboard', name: 'Dashboard', component: Dashboard, layout: Main },
+  { path: '/enrollment', name: 'enrollment', component: Page,layout: Sub },
+  { path: '/arrival', name: 'arrival',  component: Page, layout: Sub  },
+
+
+];
+
+export default page;
